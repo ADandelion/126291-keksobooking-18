08e10cc15b58a641701e09214a4dcde19b893f65
Sasha <alxblag@ya.ru>
2019-09-26 01:06:34 +0300

    Метки на карте4

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ var TYPE_OF_HOUSING = ['palace', 'flat', 'house', 'bungalo'];
 var TIME_CHECKIN = ['12:00', '13:00', '14:00'];
 var TIME_CHECKOUT = ['12:00', '13:00', '14:00'];
 var HOUSE_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var HOUSE_TITTLE = ['Красивый дома на берегу моря', 'Квартира в центре города', 'Апартаменты в живописном месте '];
 var HOUSE_PHOTO = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
 var advertisments = [];
@@ -14,10 +15,10 @@ document.querySelector('.map').classList.remove('map--faded');
 
 var markLists = document.querySelector('.map__pins');
 var markTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
-
+var mapWidth = document.querySelector('.map').offsetWidth;
 
 // Возвращает случайный элемент из массива
-var getRandomInteger = function (arr) {
+var getRandomIntegerInArray = function (arr) {
   var randInteger = Math.floor(Math.random() * arr.length);
   for (var i = 0; i < arr.length; i++) {
     var randElement = arr[randInteger];
@@ -27,11 +28,11 @@ var getRandomInteger = function (arr) {
 };
 
 // Случайное число в диапозне
-var randomIntegerRange = function (min, max) {
-  var rand = min - 0.5 + Math.random() * (max - min + 1);
-  rand = Math.round(rand);
+function randomIntegerRange(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
 
-  return rand;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 // Случайное кол-во строк из массива
@@ -46,28 +47,28 @@ var randomArrayLength = function (arrLength) {
 };
 
 // Собирает объекты в массив
-var getAllSimilarAds = function (fnRandomInteger, fnRandomArrayLength, fnRandomIntegerRange) {
+var getAllSimilarAds = function (obj) {
   for (var j = 0; j < NUMBER_ADS; j++) {
-    advertisments.push({
+    obj.push({
       'author': {
         'avatar': 'img/avatars/user0' + (j + 1) + '.png'
       },
       'offer': {
-        'tittle': 'Сдаю хату',
-        'address': '600, 300',
-        'price': '150',
-        'type': fnRandomInteger(TYPE_OF_HOUSING),
-        'rooms': '3',
-        'guests': '2',
-        'checkin': fnRandomInteger(TIME_CHECKIN),
-        'checkout': fnRandomInteger(TIME_CHECKOUT),
-        'features': fnRandomArrayLength(HOUSE_FEATURES),
-        'description': 'Красивый дом у моря',
-        'photos': fnRandomArrayLength(HOUSE_PHOTO)
+        'tittle': getRandomIntegerInArray(HOUSE_TITTLE),
+        'address': '' + randomIntegerRange(location.x, mapWidth) + ', ' + randomIntegerRange(location.y, location.y),
+        'price': randomIntegerRange(10000, 50000),
+        'type': getRandomIntegerInArray(TYPE_OF_HOUSING),
+        'rooms': randomIntegerRange(1, 3),
+        'guests':randomIntegerRange(1, 5),
+        'checkin': getRandomIntegerInArray(TIME_CHECKIN),
+        'checkout': getRandomIntegerInArray(TIME_CHECKOUT),
+        'features': randomArrayLength(HOUSE_FEATURES),
+        'description': getRandomIntegerInArray(HOUSE_TITTLE),
+        'photos': randomArrayLength(HOUSE_PHOTO)
       },
       'location': {
-        'x': fnRandomIntegerRange(130, 630),
-        'y': fnRandomIntegerRange(130, 630)
+        'x': randomIntegerRange(25, (mapWidth - 30)),
+        'y': randomIntegerRange(130, 630)
       }
     });
   }
@@ -75,14 +76,14 @@ var getAllSimilarAds = function (fnRandomInteger, fnRandomArrayLength, fnRandomI
   return advertisments;
 };
 
-getAllSimilarAds(getRandomInteger, randomArrayLength, randomIntegerRange);
+getAllSimilarAds(advertisments);
 
 // Рендер марок на карте
 var renderMarks = function (mark) {
   var markElement = markTemplate.cloneNode(true);
 
-  markElement.style.left = '' + (mark.location.x) + 'px';
-  markElement.style.top = '' + (mark.location.x) + 'px';
+  markElement.style.left = '' + (mark.location.x - 25) + 'px';
+  markElement.style.top = '' + (mark.location.y - 70) + 'px';
   markElement.querySelector('img').src = mark.author.avatar;
   markElement.querySelector('img').alt = mark.offer.tittle;
 
@@ -90,16 +91,15 @@ var renderMarks = function (mark) {
 };
 
 // Создаем DOM элементы
-var addMarksList = function (marksList) {
+var addMarksList = function (marksList, marksArr) {
   var fragment = document.createDocumentFragment();
   for (var i = 0; i < NUMBER_ADS; i++) {
-    fragment.appendChild(renderMarks(advertisments[i]));
-
+    fragment.appendChild(renderMarks(marksArr[i]));
   }
 
   return marksList.appendChild(fragment);
 };
 
 
-addMarksList(markLists);
+addMarksList(markLists, advertisments);
 
